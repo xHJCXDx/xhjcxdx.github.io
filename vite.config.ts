d41b26a3c2d7af9eeae415ba6e018d278b79859d
xHJCXDx <hirojulian@gmail.com>
2025-09-06 02:31:20 -0300

    Modification config vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: '/Portfolio_hjcxd/',
+  base: '/xhjcxdx.github.io/',
   plugins: [react()],
   resolve: {
     alias: {
