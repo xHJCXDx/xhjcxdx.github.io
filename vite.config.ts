3ca6dafa8dee79b8fc5d707a382da79693900d9c
xHJCXDx <hirojulian@gmail.com>
2025-09-06 02:42:27 -0300

    config vite V1

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: '/xhjcxdx.github.io/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
