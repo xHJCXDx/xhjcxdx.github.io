2491dfddb65f03b87891c684a21a5f14f1849f07
xHJCXDx <hirojulian@gmail.com>
2025-09-06 01:56:39 -0300

    Page

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { Navbar } from './Navbar';
-export { Footer } from './Footer';
-export * from './sections'
--- src/components/sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { Home } from './Home';
-export { About } from './About';
-export { Skills } from './Skills';
-export { Projects } from './Projects';
-export { Contact } from './Contact';
